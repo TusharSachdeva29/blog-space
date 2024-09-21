50a4a696178876b4f25f54a634504d1d56cfea28
Tushar <sachdevatushar81@gmail.com>
2024-09-21 22:34:05 +0530

    initial commit : basic template

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,3 +1,8 @@
+import { Link } from "react-router-dom";
+import InputBox from "../components/input.component";
+import googleIcon from "../imgs/google.png"
+
+
 const UserAuthForm = ({ type }) => {
     return (
         <section className="h-cover flex items-center justify-center">
@@ -7,10 +12,59 @@ const UserAuthForm = ({ type }) => {
                 </h1>
 
                 {type !== "sign-in" ? 
-                    <InputBox /> 
+                    <InputBox 
+                        name="Full-Name"
+                        type="text"
+                        placeholder="full-name"
+                        icon = "fi-rr-user"
+                        /> 
                     : 
                     ""
                 }
+                <InputBox 
+                    name="email"
+                    type="text"
+                    placeholder="email"
+                    icon = "fi-rr-envelope"
+                /> 
+                <InputBox 
+                    name="password"
+                    type="password"
+                    placeholder="password"
+                    icon = "fi-rr-key"
+                /> 
+
+                <button className="btn-dark center mt-14"
+                type = "submit">
+                    {type.replace("-"," ")}
+                </button>
+                <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-blackfont-bold">
+                    <hr className="w-1/2 border-black"/>
+                    <p>or</p>
+                    <hr className="w-1/2 border-black"/>
+                </div>
+
+                <button className = "btn-dark flex items-center justify-center gap-4 w-[90%] center">
+                    <img src={googleIcon} className="w-5" />
+                    contnue wth google
+                </button>
+                {type === "sign-in" ? (
+                    <p className="text-dark-grey text-xl text-center">
+                        Don't have an account?
+                        <Link to="/signup" className="text-black text-xl ml-1">
+                            Join us today
+                        </Link>
+                    </p>
+                ) : (
+                    <p className="text-dark-grey text-xl text-center">
+                        Already a member?
+                        <Link to="/signin" className="text-black text-xl ml-1">
+                            Sign in here.
+                        </Link>
+                    </p>
+                )}
+
+
             </form>
         </section>
     );
