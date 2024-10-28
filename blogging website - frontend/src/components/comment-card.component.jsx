a26e81815e2cd52518cfbb73c39f9ec48fe7aee1
Tushar <sachdevatushar81@gmail.com>
2024-10-28 23:11:13 +0530

    replies pagination done :

--- blogging website - frontend/src/components/comment-card.component.jsx
+++ blogging website - frontend/src/components/comment-card.component.jsx
@@ -77,20 +77,24 @@ const removeCommentsCards = (startingPoint, isDelete = false) => {
 
         }
 
-        const loadReplies = ({skip = 0}) => {
+        const loadReplies = ({skip = 0 , currentIndex = index}) => {
 
-            if(children.length){
+
+
+            if(commentsArr[currentIndex].children.length ){
                 hideReplies();
                 axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/get-replies" , {
-                    _id, skip
+                    _id : commentsArr[currentIndex]._id, skip
                 })
                 .then(({data : {replies}}) => {
-                    commentData.isReplyLoaded = true
+                    commentsArr[currentIndex].isReplyLoaded = true
                     for(let i=0;i<replies.length ; i++){
-                        replies[i].childrenLevel = commentData.childrenLevel + 1;
+                        replies[i].childrenLevel = commentsArr[currentIndex].childrenLevel + 1;
 
-                        commentsArr.splice(index +1+i+skip , 0,replies[i])
+                        commentsArr.splice(currentIndex +1+i+skip , 0,replies[i])
                     }
+
+
 
                     setBlog({...blog , comments : {...comments , results: commentsArr}})
                 })
@@ -125,6 +129,33 @@ const removeCommentsCards = (startingPoint, isDelete = false) => {
         })
 
 
+        }
+
+        const LoadMoreReplyButton = () => {
+
+            let parentIndex = getParentIndex()
+
+            let button = <button className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2"
+            onClick={() => loadReplies({ skip: index - parentIndex , currentIndex : parentIndex })}
+            >Load More Replies</button>
+
+            if(commentsArr[index + 1]){
+                if(commentsArr[index+1].childrenLevel < commentsArr[index].childrenLevel){
+
+                    if((index - parentIndex) < commentsArr[parentIndex].children.length){
+                        return button
+                    }
+                }
+            }else {
+                if(parentIndex){
+                    if((index - parentIndex) < commentsArr[parentIndex].children.length){
+                        return button
+                    }
+                }
+            }
+            return <button>
+
+            </button>
         }
         
 
@@ -192,6 +223,8 @@ const removeCommentsCards = (startingPoint, isDelete = false) => {
                 }
 
             </div>
+
+            <LoadMoreReplyButton />
 
         </div>
         
